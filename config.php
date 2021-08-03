ae67882ab394f1778ba1143754776f657396eb36
Apostolos Xenos <apostolosxenos@outlook.com>
2021-08-03 19:20:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,5 @@
 <?php
 
-// define('DB_SERVER', 'localhost');
-// define('DB_NAME', 'xenosdb');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DOMAIN', 'http://petshop.demo/');
-
-// $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3307);
-
 define('DB_SERVER', 'REPLACE_WITH_HOST_NAME_OR_IP');
 define('DB_NAME', 'REPLACE_WITH_DB_NAME');
 define('DB_USERNAME', 'REPLACE_WITH_DB_USER');
